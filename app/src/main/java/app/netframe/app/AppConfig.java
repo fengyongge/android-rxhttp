e8a19164da8849bdc84e3f7f36fd1af40113cc8c
fsuper <917386389@qq.com>
2017-06-30 16:35:03 +0800

    update
    update

--- app/src/main/java/app/netframe/app/AppConfig.java
+++ app/src/main/java/app/netframe/app/AppConfig.java
@@ -8,7 +8,7 @@ package app.netframe.app;
 public class AppConfig {
 
     public final static boolean DEBUG =true;
-    public static final String BASE_URL = "http://qiye.tsdccore.ediankai.com/sdc/v1/";
+    public static final String BASE_URL = "http://qiye.tsdccore.*.com/sdc/v1/";
     public final static String PUBLICKEY="DGAIC6F0SW5BTV56";
     public final  static String APPSECRET="D$GAS@WQK8QD19$I";
 }
