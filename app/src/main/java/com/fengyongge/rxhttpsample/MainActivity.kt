6723d4262862970b89846808e4959a4c620c6171
fyg <fengyongge98@gmail.com>
2020-10-25 23:38:43 +0800

    fix

--- app/src/main/java/com/fengyongge/rxhttpsample/MainActivity.kt
+++ app/src/main/java/com/fengyongge/rxhttpsample/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.fengyongge.rxhttpsample
 
-import android.app.Activity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Created by fengyongge on 16/5/12.
  */
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle) {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
 }
